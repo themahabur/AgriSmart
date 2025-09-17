b7bbe8da9b76a44ba5585785c6a2b60d9ee02edb
sakib11 <abcd.sakib50@gmail.com>
2025-09-17 13:57:15 +0600

    QuickAccess folder add

---
+++ src/app/componets/QuickAccess/QuickAccess.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuickAccess = () => {
+    return (
+        <div>
+            <h2 className="text-2xl font-bold text-primary">Quick Access</h2>
+        </div>
+    );
+};
+
+export default QuickAccess;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import "./globals.css";
+import QuickAccess from "./componets/QuickAccess/QuickAccess";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-primary">Welcome to Next.js!</h1>
       <p className="text-lg text-secondary">This is the home page.</p>
+      <QuickAccess />
     </div>
   );
 }
