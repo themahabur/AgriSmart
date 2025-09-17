2e50ecb810f72d31d121bb40efdcf62323356f08
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-09-17 21:45:56 +0600

    addded call to action section

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,14 @@ import QuickAccess from './components/QuickAccess/QuickAccess';
 import Feature from './components/feature/Feature';
 import Consultation from './components/consultation/Consultation';
 import Banner from './components/Banner/Banner';
+import CallToAction from './components/CallToAction/CallToAction';
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-primary">Welcome to Next.js!</h1>
       <Banner></Banner>
+      <CallToAction></CallToAction>
       <QuickAccess />
 
       {/* feature section */}
