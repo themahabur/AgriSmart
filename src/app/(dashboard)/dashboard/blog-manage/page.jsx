b99a6cc88484ce54841f817ef44feee5ea9f3379
Arifuzzaman01 <arifuzzamanar29@gmail.com>
2025-10-06 22:44:58 +0600

    change text color

--- src/app/(dashboard)/dashboard/blog-manage/page.jsx
+++ src/app/(dashboard)/dashboard/blog-manage/page.jsx
@@ -208,8 +208,8 @@ export default function AdminBlogs() {
                   onClick={() => setActiveTab(tab.key)}
                   className={`flex items-center py-4 px-6 border-b-2 font-medium  ${
                     activeTab === tab.key
-                      ? "border-blue-500 text-blue-600"
-                      : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                      ? "border-green-500 text-green-600"
+                      : "border-transparent text-green-500 hover:text-green-700 hover:border-green-300"
                   }`}
                 >
                   {tab.label}
